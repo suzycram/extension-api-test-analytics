c45035d165ed26c5e7971a106390f95f8c3da05d
suzycram <suzy@symphony.com>
2017-02-09 12:18:25 -0800

    Switch back to custom app id

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -18,10 +18,10 @@
 **/
 
 // Create our own local service pertaining to the application module
-// We have namespaced local services with "SymphonyAnalyticsApp:"
-var SymphonyAnalyticsAppAppService = SYMPHONY.services.register("SymphonyAnalyticsApp:app");
+// We have namespaced local services with "1854a949f53b49039b4a1a5038629922:"
+var 1854a949f53b49039b4a1a5038629922AppService = SYMPHONY.services.register("1854a949f53b49039b4a1a5038629922:app");
 
-SYMPHONY.remote.SymphonyAnalyticsApp().then(function(data) {
+SYMPHONY.remote.1854a949f53b49039b4a1a5038629922().then(function(data) {
 
     // Set the theme of the app module
     var themeColor = data.themeV2.name;
@@ -29,7 +29,7 @@ SYMPHONY.remote.SymphonyAnalyticsApp().then(function(data) {
     // You must add the symphony-external-app class to the body element
     document.body.className = "symphony-external-app " + themeColor + " " + themeSize;
 
-    SYMPHONY.application.connect("SymphonyAnalyticsApp", ["modules", "applications-nav", "ui", "share"], ["SymphonyAnalyticsApp:app"]).then(function(response) {
+    SYMPHONY.application.connect("1854a949f53b49039b4a1a5038629922", ["modules", "applications-nav", "ui", "share"], ["1854a949f53b49039b4a1a5038629922:app"]).then(function(response) {
 
         // The userReferenceId is an anonymized random string that can be used for uniquely identifying users.
         // The userReferenceId persists until the application is uninstalled by the user. 
@@ -44,7 +44,7 @@ SYMPHONY.remote.SymphonyAnalyticsApp().then(function(data) {
 
         // UI: Listen for theme change events
         uiService.listen("themeChangeV2", function() {
-            SYMPHONY.remote.SymphonyAnalyticsApp().then(function(data) {
+            SYMPHONY.remote.1854a949f53b49039b4a1a5038629922().then(function(data) {
                 themeColor = data.themeV2.name;
                 themeSize = data.themeV2.size;
                 document.body.className = "symphony-external-app " + themeColor + " " + themeSize;
@@ -52,16 +52,16 @@ SYMPHONY.remote.SymphonyAnalyticsApp().then(function(data) {
         });
 
         // MODULE: Add a menu item to our module
-        modulesService.addMenuItem("SymphonyAnalyticsApp", "About SymphonyAnalyticsApp World App", "SymphonyAnalyticsApp-menu-item");
-        modulesService.setHandler("SymphonyAnalyticsApp", "SymphonyAnalyticsApp:app");
+        modulesService.addMenuItem("1854a949f53b49039b4a1a5038629922", "About 1854a949f53b49039b4a1a5038629922 World App", "1854a949f53b49039b4a1a5038629922-menu-item");
+        modulesService.setHandler("1854a949f53b49039b4a1a5038629922", "1854a949f53b49039b4a1a5038629922:app");
 
        
         // Implement methods on the application module service
-        SymphonyAnalyticsAppAppService.implement({
+        1854a949f53b49039b4a1a5038629922AppService.implement({
             // If the menu item is selected, display the About text 
             menuSelect: function(itemId) {
-                if (itemId == "SymphonyAnalyticsApp-menu-item") {
-                    document.getElementById("about-SymphonyAnalyticsApp-world-app").className = "";
+                if (itemId == "1854a949f53b49039b4a1a5038629922-menu-item") {
+                    document.getElementById("about-1854a949f53b49039b4a1a5038629922-world-app").className = "";
                 }
             }
         });
